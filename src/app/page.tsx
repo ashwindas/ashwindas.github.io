38983c242cf5fd522544799a6307c809421e9175
Ashwin Das Gururaja <ashwindascg@gmail.com>
2025-03-20 23:40:55 -0700

    Add LinkedIn button to hero section and improve Email button styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,6 +42,7 @@ export default function Home() {
               >
                 Photography
               </ScrollButton>
+              <LinkedInButton />
             </div>
           </div>
         </div>
