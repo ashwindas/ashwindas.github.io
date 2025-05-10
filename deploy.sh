2a9db37f25c91af3c136fde643b1d004af4ab393
Ashwin Das Gururaja <ashwindascg@gmail.com>
2025-05-10 15:24:17 -0700

    Update site content and build

--- deploy.sh
+++ deploy.sh
@@ -60,7 +60,7 @@ echo -e "${YELLOW}Restoring CNAME file...${NC}"
 if [ -f "out/CNAME.bak" ]; then
   mv out/CNAME.bak out/CNAME
 elif [ ! -f "out/CNAME" ]; then
-  echo "ashwindas.github.io" > out/CNAME
+  echo "ashwindas.com" > out/CNAME
 fi
 
 # List files for verification
